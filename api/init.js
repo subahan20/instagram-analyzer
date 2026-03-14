29c6936944120167ef6e77b9c17c7c635034df7e
subahan <subahan.official@gmail.com>
2026-03-14 18:02:36 +0530

    webhook added

--- api/init.js
+++ api/init.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
 
   try {
 
-    const response = await fetch("https://localhost:5678/webhook/init", {
+    const response = await fetch("https://charley-unsprouted-seriously.ngrok-free.dev/webhook/init", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
